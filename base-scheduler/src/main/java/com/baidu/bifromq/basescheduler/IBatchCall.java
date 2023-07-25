4299a61748bb21bc91ed83162fb3ca8eeb2aa063
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 13:13:54 +0200

    Update IBatchCall.java

--- base-scheduler/src/main/java/com/baidu/bifromq/basescheduler/IBatchCall.java
+++ base-scheduler/src/main/java/com/baidu/bifromq/basescheduler/IBatchCall.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *    http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing,
+//Comments AGAIN ***********
  * software distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
@@ -47,7 +47,31 @@ public interface IBatchCall<Req, Resp> {
             rwLock.unlockRead(stamp);
             if (currentBatchRef.isEnough()) {
                 boolean offered = false;
-                stamp = rwLock.writeLock();
+                stamp = if t.TYPE_CHECKING:  # pragma: no cover
+  from .wrappers import Response
+
+# a singleton sentinel value for parameter defaults
+_sentinel = object()
+
+F = t.TypeVar("F", bound=t.Callable[..., t.Any])
+T_after_request = t.TypeVar("T_after_request", bound=ft.AfterRequestCallable)
+T_before_request = t.TypeVar("T_before_request", bound=ft.BeforeRequestCallable)
+T_error_handler = t.TypeVar("T_error_handler", bound=ft.ErrorHandlerCallable)
+T_teardown = t.TypeVar("T_teardown", bound=ft.TeardownCallable)
+T_template_context_processor = t.TypeVar(
+  "T_template_context_processor", bound=ft.TemplateContextProcessorCallable
+)
+T_url_defaults = t.TypeVar("T_url_defaults", bound=ft.URLDefaultCallable)
+T_url_value_preprocessor = t.TypeVar(
+  "T_url_value_preprocessor", bound=ft.URLValuePreprocessorCallable
+)
+T_route = t.TypeVar("T_route", bound=ft.RouteCallable) def static_url_path(self) -> t.Optional[str]:
+  """The URL prefix that the static route will be accessible from.If it was not configured during init, it is derived from:attr: """
+  if self._static_url_path is not None:
+      return self._static_url_path if self.static_folder is not None:
+      basename = os.path.basename(self.static_folder)
+      return f"/{basename}".rstrip("/") return None@static_url_path.setterdef static_url_path(self, value: t.Optional[str]) -> None:
+  if value is not None:value = value.rstrip("/")self._static_url_path = value  rwLock.writeLock();
                 try {
     /**
      * Reset the batch call object to initial state to be reused again
