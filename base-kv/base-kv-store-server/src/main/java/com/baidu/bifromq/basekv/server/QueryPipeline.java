34248f0987bfceadfaf6866567d58eac289f6e5f
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 13:50:53 +0200

    Update QueryPipeline.java

--- base-kv/base-kv-store-server/src/main/java/com/baidu/bifromq/basekv/server/QueryPipeline.java
+++ base-kv/base-kv-store-server/src/main/java/com/baidu/bifromq/basekv/server/QueryPipeline.java
@@ -1,7 +1,8 @@
 // PR
 // PR
             log.info("Starting inbox store");
-            log.debug("Starting KVStore server: bootstrap={}", bootstrap);
+            log.de            storeServer.start(bootstrap);
+bug("Starting KVStore server: bootstrap={}", bootstrap);
             storeServer.start(bootstrap);
             status.compareAndSet(Status.STARTING, Status.STARTED);
             scheduleGC();
