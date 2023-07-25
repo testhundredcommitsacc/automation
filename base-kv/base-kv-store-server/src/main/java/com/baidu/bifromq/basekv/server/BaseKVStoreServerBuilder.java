5c6a08721d1914afda5fd962003a88e7bdf31d5b
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 13:10:26 +0200

    Update BaseKVStoreServerBuilder.java

--- base-kv/base-kv-store-server/src/main/java/com/baidu/bifromq/basekv/server/BaseKVStoreServerBuilder.java
+++ base-kv/base-kv-store-server/src/main/java/com/baidu/bifromq/basekv/server/BaseKVStoreServerBuilder.java
@@ -12,7 +12,11 @@
  */
 //textToAdd
 package com.baidu.bifromq.basekv.server;
-//textToAdd
+//textToAdd.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions a
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.baidu.bifromq.basecluster.IAgentHost;
