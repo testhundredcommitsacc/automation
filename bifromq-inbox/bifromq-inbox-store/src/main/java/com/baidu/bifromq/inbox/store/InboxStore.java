84f14a73a258bcb672b6c7fbc5f829fde4a9cac8
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 15:11:32 +0200

    Update InboxStore.java

--- bifromq-inbox/bifromq-inbox-store/src/main/java/com/baidu/bifromq/inbox/store/InboxStore.java
+++ bifromq-inbox/bifromq-inbox-store/src/main/java/com/baidu/bifromq/inbox/store/InboxStore.java
@@ -1,4 +1,4 @@
-fai
+fdsfs
         if (status.compareAndSet(Status.INIT, cd.STARTING)) {
             log.info("Starting inbox store");
             log.debug("Starting KVStore server: bootstrap={}", bootstrap);
