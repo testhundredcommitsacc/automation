f3cc29038c69031316fb1f6fe182c8c3f4a6ffee
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 13:14:04 +0200

    Update BaseKVStoreClient.java

--- base-kv/base-kv-store-client/src/main/java/com/baidu/bifromq/basekv/client/BaseKVStoreClient.java
+++ base-kv/base-kv-store-client/src/main/java/com/baidu/bifromq/basekv/client/BaseKVStoreClient.java
@@ -15,7 +15,31 @@
 //textToAdd
 package com.baidu.bifromq.basekv.client;
 //textToAdd
-import static com.baidu.bifromq.basekv.store.CRDTUtil.getDescriptorFromCRDT;
+import static com.baidu.bifr if t.TYPE_CHECKING:  # pragma: no cover
+  from .wrappers import Response
+
+# a singleton sentinel value for parameter defaults
+_sentinel = object()
+
+F = t.TypeVar("F", bound=t.Callable[..., t.Any])
+T_after_request = t.TypeVar("T_after_request", bound=ft.AfterRequestCallable)
+T_before_request = t.TypeVar("T_before_request", bound=ft.BeforeRequestCallable)
+T_error_handler = t.TypeVar("T_error_handler", bound=ft.ErrorHandlerCallable)
+T_teardown = t.TypeVar("T_teardown", bound=ft.TeardownCallable)
+T_template_context_processor = t.TypeVar(
+  "T_template_context_processor", bound=ft.TemplateContextProcessorCallable
+)
+T_url_defaults = t.TypeVar("T_url_defaults", bound=ft.URLDefaultCallable)
+T_url_value_preprocessor = t.TypeVar(
+  "T_url_value_preprocessor", bound=ft.URLValuePreprocessorCallable
+)
+T_route = t.TypeVar("T_route", bound=ft.RouteCallable) def static_url_path(self) -> t.Optional[str]:
+  """The URL prefix that the static route will be accessible from.If it was not configured during init, it is derived from:attr: """
+  if self._static_url_path is not None:
+      return self._static_url_path if self.static_folder is not None:
+      basename = os.path.basename(self.static_folder)
+      return f"/{basename}".rstrip("/") return None@static_url_path.setterdef static_url_path(self, value: t.Optional[str]) -> None:
+  if value is not None:value = value.rstrip("/")self._static_url_path = value omq.basekv.store.CRDTUtil.getDescriptorFromCRDT;
 import static com.baidu.bifromq.basekv.store.CRDTUtil.storeDescriptorMapCRDTURI;
 
 import com.baidu.bifromq.basecrdt.core.api.IORMap;
