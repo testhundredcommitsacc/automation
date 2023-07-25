82b4d50c32aa4dbd0cda7697fd4a51636ce7cc27
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 13:02:54 +0200

    Update ICallScheduler.java

--- base-scheduler/src/main/java/com/baidu/bifromq/basescheduler/ICallScheduler.java
+++ base-scheduler/src/main/java/com/baidu/bifromq/basescheduler/ICallScheduler.java
@@ -1,4 +1,4 @@
-textToAdd
+INPUT TEXT ***************
  * Copyright (c) 2023. Baidu, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Versio * Licensed under the Apache License, Version 2.0 (the "License");
