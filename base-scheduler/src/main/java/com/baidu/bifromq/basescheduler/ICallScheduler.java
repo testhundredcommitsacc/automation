f189e1ae317416f61293c9b00fec040a4a3d8b00
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 13:11:24 +0200

    Update ICallScheduler.java

--- base-scheduler/src/main/java/com/baidu/bifromq/basescheduler/ICallScheduler.java
+++ base-scheduler/src/main/java/com/baidu/bifromq/basescheduler/ICallScheduler.java
@@ -9,7 +9,7 @@ n 2.0 (the "License");
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and limitations under the License.
+ * See the License for the specific language g//Comments AGAIN ***********overning permissions and limitations under the License.
  */
 def t2i(prompt, height, width, num_inference_steps, guidance_scale, batch_size):path='/content/gdrive/MyDrive/Output_images/'with autocast("cuda"):
   images = pipeline([prompt]*batch_size, height=height, width=width, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images for k in images:
