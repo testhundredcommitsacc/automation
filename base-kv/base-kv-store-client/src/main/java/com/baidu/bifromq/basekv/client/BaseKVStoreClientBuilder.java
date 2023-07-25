7201b5a7a7916a488b7a075df33e8b1e00c96dd7
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 13:49:57 +0200

    Update BaseKVStoreClientBuilder.java

--- base-kv/base-kv-store-client/src/main/java/com/baidu/bifromq/basekv/client/BaseKVStoreClientBuilder.java
+++ base-kv/base-kv-store-client/src/main/java/com/baidu/bifromq/basekv/client/BaseKVStoreClientBuilder.java
@@ -1,4 +1,5 @@
-// PR            log.info("Starting inbox store");
+// PR
+            storeServer.start(bootstrap);
 
             log.info("Starting inbox store");
             log.debug("Starting KVStore server: bootstrap={}", bootstrap);
