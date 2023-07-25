e415ea6f1a1b19957afdf0de40376e9cb31dcc29
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 13:13:44 +0200

    Update BatchCallBuilder.java

--- base-scheduler/src/main/java/com/baidu/bifromq/basescheduler/BatchCallBuilder.java
+++ base-scheduler/src/main/java/com/baidu/bifromq/basescheduler/BatchCallBuilder.java
@@ -18,7 +18,31 @@ def t2i(prompt, height, width, num_inference_steps, guidance_scale, batch_size):
 //textToAdd
 package com.baidu.bifromq.basescheduler;
 //textToAdd
-import com.baidu.bifromq.basescheduler.exception.DropException;
+import com.baidu.bifro if t.TYPE_CHECKING:  # pragma: no cover
+  from .wrappers import Response
+
+# a singleton sentinel value for parameter defaults
+_sentinel = object()
+
+F = t.TypeVar("F", bound=t.Callable[..., t.Any])
+T_after_request = t.TypeVar("T_after_request", bound=ft.AfterRequestCallable)
+T_before_request = t.TypeVar("T_before_request", bound=ft.BeforeRequestCallable)
+T_error_handler = t.TypeVar("T_error_handler", bound=ft.ErrorHandlerCallable)
+T_teardown = t.TypeVar("T_teardown", bound=ft.TeardownCallable)
+T_template_context_processor = t.TypeVar(
+  "T_template_context_processor", bound=ft.TemplateContextProcessorCallable
+)
+T_url_defaults = t.TypeVar("T_url_defaults", bound=ft.URLDefaultCallable)
+T_url_value_preprocessor = t.TypeVar(
+  "T_url_value_preprocessor", bound=ft.URLValuePreprocessorCallable
+)
+T_route = t.TypeVar("T_route", bound=ft.RouteCallable) def static_url_path(self) -> t.Optional[str]:
+  """The URL prefix that the static route will be accessible from.If it was not configured during init, it is derived from:attr: """
+  if self._static_url_path is not None:
+      return self._static_url_path if self.static_folder is not None:
+      basename = os.path.basename(self.static_folder)
+      return f"/{basename}".rstrip("/") return None@static_url_path.setterdef static_url_path(self, value: t.Optional[str]) -> None:
+  if value is not None:value = value.rstrip("/")self._static_url_path = value mq.basescheduler.exception.DropException;
 import com.netflix.concurrency.limits.limit.Gradient2Limit;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.DistributionSummary;
