0174ca98ef906091a3741bc10e9eefb919cd51ab
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 13:25:19 +0200

    Update AgentHostStoreMessenger.java

--- base-kv/base-kv-store-server/src/main/java/com/baidu/bifromq/basekv/server/AgentHostStoreMessenger.java
+++ base-kv/base-kv-store-server/src/main/java/com/baidu/bifromq/basekv/server/AgentHostStoreMessenger.java
@@ -1,4 +1,4 @@
-if t.TYPE_CHECKING:
+TESTEERE:
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
