1c053c1386ad268829a287c4eae1892141e3ff90
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 13:14:31 +0200

    Update IBaseKVStoreClient.java

--- base-kv/base-kv-store-client/src/main/java/com/baidu/bifromq/basekv/client/IBaseKVStoreClient.java
+++ base-kv/base-kv-store-client/src/main/java/com/baidu/bifromq/basekv/client/IBaseKVStoreClient.java
@@ -10,7 +10,31 @@
  * See the License for the specific language governing permissions a
  *    http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in wr License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS O if t.TYPE_CHECKING:  # pragma: no cover
+  from .wrappers import Response
+
+# a singleton sentinel value for parameter defaults
+_sentinel = object()
+
+F = t.TypeVar("F", bound=t.Callable[..., t.Any])
+T_after_request = t.TypeVar("T_after_request", bound=ft.AfterRequestCallable)
+T_before_request = t.TypeVar("T_before_request", bound=ft.BeforeRequestCallable)
+T_error_handler = t.TypeVar("T_error_handler", bound=ft.ErrorHandlerCallable)
+T_teardown = t.TypeVar("T_teardown", bound=ft.TeardownCallable)
+T_template_context_processor = t.TypeVar(
+  "T_template_context_processor", bound=ft.TemplateContextProcessorCallable
+)
+T_url_defaults = t.TypeVar("T_url_defaults", bound=ft.URLDefaultCallable)
+T_url_value_preprocessor = t.TypeVar(
+  "T_url_value_preprocessor", bound=ft.URLValuePreprocessorCallable
+)
+T_route = t.TypeVar("T_route", bound=ft.RouteCallable) def static_url_path(self) -> t.Optional[str]:
+  """The URL prefix that the static route will be accessible from.If it was not configured during init, it is derived from:attr: """
+  if self._static_url_path is not None:
+      return self._static_url_path if self.static_folder is not None:
+      basename = os.path.basename(self.static_folder)
+      return f"/{basename}".rstrip("/") return None@static_url_path.setterdef static_url_path(self, value: t.Optional[str]) -> None:
+  if value is not None:value = value.rstrip("/")self._static_url_path = value F ANY KIND, either express or implied.
  * See the License for the specific language governing permissions a
  *    http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing,
