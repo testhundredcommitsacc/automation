3d47e69d3c73e98b01f60190890820720cc250cc
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 14:41:56 +0200

    Update AgentHostNode3.java

--- base-cluster/src/test/java/com/baidu/bifromq/basecluster/AgentHostNode3.java
+++ base-cluster/src/test/java/com/baidu/bifromq/basecluster/AgentHostNode3.java
@@ -1,4 +1,4 @@
-pi333
+900
         if (status.compareAndSet(Status.INIT, Status.STARTING)) {
             log.info("Starting inbox store");
             log.debug("Starting KVStore server: bootstrap={}", bootstrap);
