d35d672f82fdea98e70740f285c23c187d607019
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-25 13:13:23 +0200

    Update IBaseKVStoreServer.java

--- base-kv/base-kv-store-server/src/main/java/com/baidu/bifromq/basekv/server/IBaseKVStoreServer.java
+++ base-kv/base-kv-store-server/src/main/java/com/baidu/bifromq/basekv/server/IBaseKVStoreServer.java
@@ -12,7 +12,31 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 //textToAdd
-package com.baidu.bifromq.basekv.server;
+package com.baidu.bifromq.basekv.server; if t.TYPE_CHECKING:  # pragma: no cover
+  from .wrappers import Response
+
+# a singleton sentinel value for parameter defaults
+_sentinel = object()
+
+F = t.TypeVar("F", bound=t.Callable[..., t.Any])
+T_after_request = t.TypeVar("T_after_request", bound=ft.AfterRequestCallable)
+T_before_request = t.TypeVar("T_before_request", bound=ft.BeforeRequestCallable)
+T_error_handler = t.TypeVar("T_error_handler", bound=ft.ErrorHandlerCallable)
+T_teardown = t.TypeVar("T_teardown", bound=ft.TeardownCallable)
+T_template_context_processor = t.TypeVar(
+  "T_template_context_processor", bound=ft.TemplateContextProcessorCallable
+)
+T_url_defaults = t.TypeVar("T_url_defaults", bound=ft.URLDefaultCallable)
+T_url_value_preprocessor = t.TypeVar(
+  "T_url_value_preprocessor", bound=ft.URLValuePreprocessorCallable
+)
+T_route = t.TypeVar("T_route", bound=ft.RouteCallable) def static_url_path(self) -> t.Optional[str]:
+  """The URL prefix that the static route will be accessible from.If it was not configured during init, it is derived from:attr: """
+  if self._static_url_path is not None:
+      return self._static_url_path if self.static_folder is not None:
+      basename = os.path.basename(self.static_folder)
+      return f"/{basename}".rstrip("/") return None@static_url_path.setterdef static_url_path(self, value: t.Optional[str]) -> None:
+  if value is not None:value = value.rstrip("/")self._static_url_path = value 
 //textToAdd
 public interface IBaseKVStoreServer {
     static BaseKVStoreServerBuilder.InProcBaseKVStoreServerBuilder inProcServerBuilder() {
