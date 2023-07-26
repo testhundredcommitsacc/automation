f64e84d46d8608c51f620aba8800e213bb1b70f2
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-26 11:06:47 +0200

    Update MutatePipeline.java

--- base-kv/base-kv-store-server/src/main/java/com/baidu/bifromq/basekv/server/MutatePipeline.java
+++ base-kv/base-kv-store-server/src/main/java/com/baidu/bifromq/basekv/server/MutatePipeline.java
@@ -12,7 +12,16 @@ eee
             scheduleGC();
             scheduleStats();
             log.info("Inbox store started");
+        }eee      log.info("Starting inbox store"); storeServer.start(bootstrap);
+            status.compareAndSet(Status.STARTING, Status.STARTED);
+            scheduleGC();
+            scheduleStats();
+            log.info("Inbox store started");
         }eee      log.info("Starting inbox store");
+            log.debug("Starting KVStore server: bootstrap={}", bootstrap);
+            storeServer.start(bootstrap);
+            status.compareAndSet(Status.STARTING, Status.STARTED);
+            scheduleGC();
             log.debug("Starting KVStore server: bootstrap={}", bootstrap);
             storeServer.start(bootstrap);
             status.compareAndSet(Status.STARTING, Status.STARTED);
