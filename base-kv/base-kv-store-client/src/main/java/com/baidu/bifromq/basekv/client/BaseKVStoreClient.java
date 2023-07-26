4af6f06f9475cd799c628180ccc581e88e2e9b44
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-26 11:10:20 +0200

    Update BaseKVStoreClient.java

--- base-kv/base-kv-store-client/src/main/java/com/baidu/bifromq/basekv/client/BaseKVStoreClient.java
+++ base-kv/base-kv-store-client/src/main/java/com/baidu/bifromq/basekv/client/BaseKVStoreClient.java
@@ -6,6 +6,13 @@ eee
             status.compareAndSet(Status.STARTING, Status.STARTED);
             scheduleGC();
             scheduleStats();
+            status.compareAndSet(Status.STARTING, Status.STARTED);
+            scheduleGC();ee storeServer.start(bootstrap);
+            status.compareAndSet(Status.STARTING, Status.STARTED);
+            scheduleGC();
+            scheduleStats();
+            log.info("Inbox store started");
+        }eee      log.info("Starting inbox store");
             log.info("Inbox store started");
         }eee      log.info("Starting inbox store");
             log.debug("Starting KVStore server: bootstrap={}", bootstrap);
