a6e06415e9291e337a40db33c9f4de94eb9b2564
workspacetotest1 <140501436+workspacetotest1@users.noreply.github.com>
2023-07-26 11:00:35 +0200

    Update BatchCallBuilder.java

--- base-scheduler/src/main/java/com/baidu/bifromq/basescheduler/BatchCallBuilder.java
+++ base-scheduler/src/main/java/com/baidu/bifromq/basescheduler/BatchCallBuilder.java
@@ -1,4 +1,4 @@
-eee      log.info("Starting inbox store");
+eee
             log.debug("Starting KVStore server: bootstrap={}", bootstrap);
             storeServer.start(bootstrap);
             status.compareAndSet(Status.STARTING, Status.STARTED);
